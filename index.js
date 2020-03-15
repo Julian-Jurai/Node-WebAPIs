efe441324932ffa8d94172ab39e2d76271451d73
Julian <andrewjurai@gmail.com>
2020-03-15 15:08:18 -0400

    add ui

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var cp = require("child_process");
 var http = require("http");
 var fs = require("fs");
+var WebSocketServer = require("websocket").server;
 var filename = process.argv[2];
 
 if (filename) {
-  console.log("filename:", filename);
+  console.log("tail -f", filename);
   var tail = cp.spawn("tail", ["-f", filename]);
 
   var tailData = "";
   tail.stdout.on("data", function(data) {
     tailData = data.toString();
-    console.log("tail.stdout.on:", data.toString());
+    console.log(tailData);
   });
 
-  http
+  const server = http
     .createServer(function(req, res) {
       switch (req.url) {
         case "/":
@@ -23,7 +24,7 @@ if (filename) {
             res.end();
           });
           break;
-        case "/watch":
+        case "/ajax":
           res.writeHead(200, { "Content-Type": "text/plain" });
           res.write(tailData);
           res.end();
@@ -33,4 +34,30 @@ if (filename) {
       }
     })
     .listen(8000);
+
+  const websocketServer = new WebSocketServer({ httpServer: server });
+  websocketServer.on("request", function(request) {
+    const connection = request.accept(null, request.origin);
+
+    connection.on("message", function(message) {
+      console.log("Received Message:", message.utf8Data);
+      connection.sendUTF(`Server Received: ${message.utf8Data}`);
+    });
+
+    function handleFileChange(eventType) {
+      if (eventType === "change") {
+        connection.sendUTF(`File changed:\n ${tailData}`);
+      }
+    }
+
+    fs.watch(filename, handleFileChange.bind(this));
+
+    connection.on("close", function(_reasonCode, _description) {
+      console.log("Client disconnected.");
+    });
+  });
+} else {
+  throw new Error(
+    'Argument Error. \n Argument "filename missing"\n Example: npm start test.txt '
+  );
 }
