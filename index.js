da465b4f7a2eb29128dd99e39a69104acb08cd44
Julian <andrewjurai@gmail.com>
2020-03-14 21:23:34 -0400

    implemented ajax polling

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 var cp = require("child_process");
 var http = require("http");
+var fs = require("fs");
 var filename = process.argv[2];
 
 if (filename) {
   console.log("filename:", filename);
   var tail = cp.spawn("tail", ["-f", filename]);
 
-  var body = "";
+  var tailData = "";
   tail.stdout.on("data", function(data) {
-    body = data.toString();
+    tailData = data.toString();
     console.log("tail.stdout.on:", data.toString());
   });
 
   http
     .createServer(function(req, res) {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.write(body);
-      res.end();
+      switch (req.url) {
+        case "/":
+          fs.readFile("index.html", function(err, html) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(html);
+            res.end();
+          });
+          break;
+        case "/watch":
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.write(tailData);
+          res.end();
+          break;
+        default:
+          console.log("Sorry no route matches", req.url);
+      }
     })
     .listen(8000);
 }
